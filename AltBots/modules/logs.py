f59eb78108e1fb21060e2706e5895e9d41259009
Radha xD <133753917+Radhak8@users.noreply.github.com>
2023-07-10 13:24:24 +0530

    Update logs.py

--- AltBots/modules/logs.py
+++ AltBots/modules/logs.py
@@ -42,7 +42,7 @@ async def logs(legend):
         fetch = await legend.reply(f"__Fetching Logs...__")
     
         with open("AltLogs.txt", "w") as logfile:
-            logfile.write("⚡ XBOTS ⚡ [ Bot Logs ]\n\n" + logs)
+            logfile.write("⚡ BOTXSPAM ⚡ [ Bot Logs ]\n\n" + logs)
 
         end = datetime.now()
         ms = (end-start).seconds
