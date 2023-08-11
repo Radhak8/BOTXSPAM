f64636abfcbcd4f2b38c10fbba591755f2979a79
Radha xD <133753917+Radhak8@users.noreply.github.com>
2023-08-11 09:33:29 +0530

    🥂

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -8,7 +8,7 @@ START_BUTTON = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
     ],
     [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/RadhaXUpdate"),
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/RadhaX2Update"),
         Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/RadhaX2Support")
     ],
     [
