a1777f808671d9d2938ef2a08cf5cd26d0434990
Radha xD <133753917+Radhak8@users.noreply.github.com>
2023-08-13 12:46:36 +0530

    🖤

--- AltBots/modules/help.py
+++ AltBots/modules/help.py
@@ -3,7 +3,7 @@ from telethon import events, Button
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
 
 
-HELP_STRING = f"★ 𝘽𝙤𝙩𝗦𝗽𝗮𝗺 𝙃𝙚𝙡𝙥 𝙈𝙚𝙣𝙪 ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @GhostRadha**"
+HELP_STRING = f"ʙᴏᴛxꜱᴩᴀᴍ ʜᴇʟᴩ ᴍᴇɴᴜ ⚡\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @GhostRadha**"
 
 HELP_BUTTON = [
     [
